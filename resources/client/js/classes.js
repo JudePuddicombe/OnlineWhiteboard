826286bffb1152c8bddc7ade6ee7f850fb4e05e4
Jude <93465@farnborough.ac.uk>
2020-12-15 12:01:53 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	resources/database.db

--- resources/client/js/classes.js
+++ resources/client/js/classes.js
@@ -213,7 +213,6 @@ class Chatboard{
 
 }
 
-
 class ChatroomServer{
 
 }
--- resources/client/js/script.js
+++ resources/client/js/script.js
@@ -12,19 +12,19 @@ function Start(){
 
     //chatboard object instantiation
 
-    let chatboardDiv = document.getElementById("chatboard");
+    let chatboardDiv = document.getElementById("chatboardText");
 
     let chatboard = new Chatboard(chatboardDiv,"This is supposed to be a server object");
 
     //pen events
 
-    document.addEventListener('mousemove', function (event) {pen.moveTo(pen.findPosition(event))});
+    canvas.addEventListener('mousemove', function (event) {pen.moveTo(pen.findPosition(event))});
     document.addEventListener('mousedown', function () {pen.down()}); //these functions are wrapped to retain "this"
     document.addEventListener('mouseup', function () {pen.up()});
 
     //automatic update
 
-    window.setInterval(function(){whiteboardServer.getWhiteboardEvents().then(events => {whiteboard.handleWhiteboardEvents(events)})}, 250);
+    // window.setInterval(function(){whiteboardServer.getWhiteboardEvents().then(events => {whiteboard.handleWhiteboardEvents(events)})}, 250);
 
     //whiteboard buttons
 
@@ -32,7 +32,7 @@ function Start(){
     document.getElementById("greenColorButton").onclick = function() {pen.setColor("green")};
     document.getElementById("blueColorButton").onclick = function() {pen.setColor("blue")};
     document.getElementById("clearWhiteboardButton").onclick = function(){whiteboard.handleClientWhiteboardEvent({type: "clear"})}; //generates clearing events for the whiteboard
-    document.getElementById("updateButton").onclick = function(){whiteboardServer.getWhiteboardEvents().then(events => {whiteboard.handleWhiteboardEvents(events)})} //gets then draw new events from the server
+    document.getElementById("updateButton").onclick = function(){whiteboardServer.getWhiteboardEvents().then(events => {whiteboard.handleWhiteboardEvents(events)})}//gets then draw new events from the server
 
     //chat buttons
 
